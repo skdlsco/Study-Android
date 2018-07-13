5cbc2ff408f354f18f861aa7c1e96b1a0933fbc4
skdlsco <nicnic1234@naver.com>
2018-07-13 15:00:28 +0900

    fion

--- app/src/main/java/com/eka/databinding/MainActivity.kt
+++ app/src/main/java/com/eka/databinding/MainActivity.kt
@@ -8,15 +8,15 @@ import com.eka.databinding.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     var item: Item = Item()
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding // lateinit 나중에 init한다는거임 ㅇㅅㅇ init안하고 쓰면 터짐
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main) // bind
 
-        item.text = "asdfadsf"
+        item.text = "asdfadsf" // item
 
-        binding.item = item
-        binding.notifyChange()
+        binding.item = item //item 연결
+        binding.notifyChange()  // notify
     }
 }
--- app/src/main/java/com/eka/databinding/MyBindingAdapter.kt
+++ app/src/main/java/com/eka/databinding/MyBindingAdapter.kt
@@ -3,11 +3,13 @@ package com.eka.databinding
 import android.databinding.BindingAdapter
 import android.widget.TextView
 
+
+// object -> 전부 다 Companion
 object MyBindingAdapter {
 
-    @JvmStatic
-    @BindingAdapter("asdf")
-    fun asdf(view: TextView, text: String) {
+    @JvmStatic // 필수
+    @BindingAdapter("asdf") // attr에서 쓰일 이름 넣기
+    fun asdf(view: TextView, text: String) { // 함수 이름 무관, view 뒤에는 받아올 인자 값 다중은 잘 기억이 안난다.
         view.text = "내용: $text"
     }
 
