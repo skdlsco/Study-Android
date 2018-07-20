cf7fd781b2f47915ea5724c5bfb2a1faf1b92aba
skdlsco <nicnic1234@naver.com>
2018-07-20 22:13:43 +0900

    get picture

--- app/src/main/java/com/eka/camerapreview/CameraView.kt
+++ app/src/main/java/com/eka/camerapreview/CameraView.kt
@@ -1,10 +1,17 @@
 package com.eka.camerapreview
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Rect
+import android.graphics.YuvImage
 import android.hardware.Camera
 import android.util.AttributeSet
+import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import java.io.ByteArrayOutputStream
+
 
 class CameraView : SurfaceView, SurfaceHolder.Callback {
 
@@ -39,7 +46,80 @@ class CameraView : SurfaceView, SurfaceHolder.Callback {
             setDisplayOrientation(90)
             setPreviewDisplay(surfaceHolder)
             startPreview()
-            this.Size(2560, 1440)
+            val params = parameters
+            val sizes = params.supportedPreviewSizes
+            getOptimumSize().let {
+                params.setPictureSize(it.width, it.height)
+            }
+            params.setPreviewSize(sizes.first().width, sizes.first().height)
+            parameters = params
+        }
+    }
+
+    private fun getOptimumSize(): Camera.Size {
+        var ratioWidth = 0
+        var ratioHeight = 0
+        mCamera?.run {
+            val params = parameters
+            var biggestWidth = params.supportedPreviewSizes.first().width
+            var biggestHeight = params.supportedPreviewSizes.first().height
+
+            var i = 2
+            while (i < biggestHeight) {
+                val hResult = biggestHeight % i
+                val wResult = biggestWidth % i
+                if (hResult == 0 && wResult == 0) {
+                    biggestHeight /= i
+                    biggestWidth /= i
+                } else
+                    i++
+            }
+            ratioHeight = biggestHeight
+            ratioWidth = biggestWidth
+
+            params.supportedPictureSizes.forEach {
+                var width = it.width
+                var height = it.height
+
+                i = 2
+                while (i < height) {
+                    val hResult = height % i
+                    val wResult = width % i
+                    if (hResult == 0 && wResult == 0) {
+                        height /= i
+                        width /= i
+                    } else
+                        i++
+                }
+                if (width == ratioWidth && height == ratioHeight) {
+                    return it
+
+                }
+
+            }
+        }
+        return mCamera!!.parameters.supportedPreviewSizes.first()
+    }
+
+    fun getPicture(callback: (img: ByteArray) -> Unit) {
+        mCamera?.takePicture(null, null) { biteArray, camera ->
+
+//            val params = mCamera!!.parameters
+//            val w = params.previewSize.width
+//            val h = params.previewSize.height
+//            val format = params.previewFormat
+//
+//            val image = YuvImage(biteArray, format, w, h, null)
+//            val out = ByteArrayOutputStream()
+//            val area = Rect(0, 0, w, h)
+//
+//            image.compressToJpeg(area, 50, out)
+
+//            callback.invoke(BitmapFactory.decodeByteArray(out.toByteArray(), 0, out.size()))
+
+//            val bmp = BitmapFactory.decodeByteArray(biteArray, 0, biteArray.size)
+
+            callback.invoke(biteArray)
         }
     }
 }
--- app/src/main/java/com/eka/camerapreview/MainActivity.kt
+++ app/src/main/java/com/eka/camerapreview/MainActivity.kt
@@ -2,12 +2,17 @@ package com.eka.camerapreview
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.SurfaceHolder
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +21,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         requestPermission()
+        button.setOnClickListener {
+            cameraView.getPicture {
+                //                startActivity(Intent(this@MainActivity, ImageActivity::class.java).apply {
+//                    putExtra("img", it)
+//                })
+
+                image.visibility = View.VISIBLE
+                image.setImageBitmap(imgRotate(makeBitmapFromByteArray(it), 90f))
+
+            }
+        }
     }
 
-    fun startCamera() {
+    private fun startCamera() {
         val holder = cameraView.holder
         holder.addCallback(cameraView)
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
@@ -42,5 +58,21 @@ class MainActivity : AppCompatActivity() {
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    fun imgRotate(bitmap: Bitmap, rotate: Float): Bitmap {
+        val width = bitmap.width
+        val height = bitmap.height
+        val matrix = Matrix()
+        matrix.postRotate(rotate)
+
+        var rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true)
+        bitmap.recycle()
+        return rotatedBitmap
+    }
+
+    fun makeBitmapFromByteArray(byteArray: ByteArray): Bitmap {
+        val bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+        return bmp
     }
 }
